8de365232d3370ef9ea45ca876c9a58ea49754dd
Carlo Landmeter <clandmeter@gmail.com>
2015-05-15 14:29:13 +0000

    change font to Open Sans

--- template.php
+++ template.php
@@ -4,3 +4,7 @@
  * @file
  * template.php
  */
+
+function alpine_bootstrap_preprocess_html(&$variables) {
+    drupal_add_css('http://fonts.googleapis.com/css?family=Open+Sans', array('type' => 'external'));
+}
