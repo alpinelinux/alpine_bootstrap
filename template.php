7366c9b05b33548d2571aa7fae3d997dc1265e04
Carlo Landmeter <clandmeter@gmail.com>
2015-05-15 14:44:56 +0000

    add bootstrap rounded images for avatars

--- template.php
+++ template.php
@@ -8,3 +8,8 @@
 function alpine_bootstrap_preprocess_html(&$variables) {
     drupal_add_css('http://fonts.googleapis.com/css?family=Open+Sans', array('type' => 'external'));
 }
+
+/* add boostrap image circle to all images */
+function alpine_bootstrap_preprocess_image(&$variables) {
+    $variables['attributes']['class'][] = 'img-circle';
+}
